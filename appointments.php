48d71a49affe888aeb1804fcf6a2e3f561e2b12b
Zanis3 <euvertzionpagad@gmail.com>
2024-05-07 15:16:40 +0800

    header

---
+++ appointments.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Appointment</title>
+    <link rel="stylesheet" href="css/style_general.css">
+    <link rel="stylesheet" href="css/style_appointment.css">
+</head>
+<body>
+    
+</body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,11 @@
+<div class="header">
+        <div class="website-logo">
+            Paws Veterinary
+        </div>
+        <div class="navbar">
+            <a href="services.php">Services</a>
+            <a href="">Our Mission</a>
+            <a href="appointments.php">Appointments</a>
+            <a href="" id="get-support">Get Support</a>
+        </div>
+</div>
