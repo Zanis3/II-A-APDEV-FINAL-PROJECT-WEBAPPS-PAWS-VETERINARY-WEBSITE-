62132949ce00c6eec88c46c6b0d6651da9e037f4
birdb3446 <birdb3446@gmail.com>
2024-05-19 18:35:31 +0800

    Update - Sacayle

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <head>
     <title>Paws Veterinary</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="css/style_general.css">
     <link rel="stylesheet" href="css/style_home.css">
 </head>
--- template/header.php
+++ template/header.php
@@ -4,9 +4,15 @@
             <h1>Paws Veterinary</h1>
         </div>
         <div class="navbar">
-            <a href="services.php">Services</a>
-            <a href="">Our Mission</a>
-            <a href="appointments.php">Appointments</a>
-            <a href="" id="get-support">Get Support</a>
+            <a href="index.php"><b> Home </b></a>
+            <a href="services.php"><b> Services </b></a>
+            <a href="appointments.php"><b> Set Appointment </b></a>
+            <a href="about.php"><b> About Us </b></a>
+            <form action="/search" method="GET">
+                    <input type="text" id="txtsearch" name="search" placeholder="Search...">
+                    <div class="icon-container">
+                        <i class="fas fa-search"></i>
+                    </div>
+            </form>
         </div>
 </div>
