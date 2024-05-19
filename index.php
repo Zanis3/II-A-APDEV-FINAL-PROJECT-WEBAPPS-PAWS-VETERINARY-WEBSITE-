71c72f3468ff793e7aa13ec7b01c47b1f8238548
Zanis3 <euvertzionpagad@gmail.com>
2024-05-19 21:41:19 +0800

    check

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="css/style_general.css">
     <link rel="stylesheet" href="css/style_home.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script href="script/script.js" deter></script>
+    <link rel="shortcut icon" href="img/gen/web-logo.png" type="image/png">
     
 </head>
 <body>
