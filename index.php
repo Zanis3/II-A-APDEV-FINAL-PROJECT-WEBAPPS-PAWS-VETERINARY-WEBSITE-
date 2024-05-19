e4bc9086f031f2e9a0ea74220b45d5436a1089e9
Zanis3 <euvertzionpagad@gmail.com>
2024-05-19 21:32:54 +0800

    par pa check

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <title>Paws Veterinary</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="css/style_general.css">
     <link rel="stylesheet" href="css/style_home.css">
-    <link rel="stylesheet" href="script/script.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    
 </head>
 <body>
-    <?php include_once 'template/header.php';?>
+    <?php 
+        include_once 'template/header.php';
+    ?>
+
+    <div class="hero-container">
+        <div class="hero-text">
+            <h1 id="hero-title">Welcome to Paws Veterinary Clinic</h1>
+            <p id="hero-sub">Providing quality care for your pets.</p>
+        </div>
+    </div>
+    
 </body>
+</html>
---
+++ template/hero.php
@@ -0,0 +1,6 @@
+<div class="hero-container">
+        <div class="hero-text">
+            <h1 id="hero-title">Welcome to Paws Veterinary Clinic</h1>
+            <p id="hero-sub">Providing quality care for your pets.</p>
+        </div>
+    </div>
