aa4850d9ceef4e2bb342fe9bda2dec57fea47596
birdb3446 <birdb3446@gmail.com>
2024-06-01 22:30:43 +0800

    Changes

--- appointment-templates/grooming.php
+++ appointment-templates/grooming.php
@@ -1,18 +1,19 @@
 <?php
+    $location = 'dashboard';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/style_general.css">
-    <link rel="stylesheet" href="../../css/style_appointment.css">
-    <link rel="shortcut icon" href="../../img/gen/web-logo.png" type="image/png">
-    <title>Grooming/title>
+    <link rel="stylesheet" href="../css/style_general.css">
+    <link rel="stylesheet" href="../css/style_appointment.css">
+    <link rel="shortcut icon" href="img/gen/web-logo.png" type="image/png">
+    <title>Grooming</title>
 </head>
 <body>
-    <?php include_once '../header.php';?>
+    <?php include_once '../template/header.php';?>
 
-    <?php include_once '../footer.php';?>
+    <?php include_once '../template/footer.php';?>
 </body>
 </html>
