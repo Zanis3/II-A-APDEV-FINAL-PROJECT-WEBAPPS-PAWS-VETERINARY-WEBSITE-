d3a9d0d771cf5c0598f864aef62fbb16eab34a98
birdb3446 <birdb3446@gmail.com>
2024-06-01 22:17:57 +0800

    Changes - appointment

--- template/appointment-templates/checkup.php
+++ template/appointment-templates/checkup.php
@@ -5,14 +5,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style_general.css">
-    <link rel="stylesheet" href="../css/style_appointment.css">
-    <link rel="shortcut icon" href="../img/gen/web-logo.png" type="image/png">
+    <link rel="stylesheet" href="../../css/style_general.css">
+    <link rel="stylesheet" href="../../css/style_appointment.css">
+    <link rel="shortcut icon" href="../../img/gen/web-logo.png" type="image/png">
     <title>Check Up</title>
 </head>
 <body>
-    <?php include_once 'header.php';?>
+    <?php include_once '../header.php';?>
 
-    <?php include_once 'footer.php';?>
+    <?php include_once '../footer.php';?>
 </body>
 </html>
--- template/appointment-templates/grooming.php
+++ template/appointment-templates/grooming.php
@@ -5,13 +5,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style_general.css">
-    <link rel="stylesheet" href="css/style_appointment.css">
-    <title>Grooming</title>
+    <link rel="stylesheet" href="../../css/style_general.css">
+    <link rel="stylesheet" href="../../css/style_appointment.css">
+    <link rel="shortcut icon" href="../../img/gen/web-logo.png" type="image/png">
+    <title>Grooming/title>
 </head>
 <body>
-    <?php include_once 'header.php';?>
+    <?php include_once '../header.php';?>
 
-    <?php include_once 'footer.php';?>
+    <?php include_once '../footer.php';?>
 </body>
 </html>
