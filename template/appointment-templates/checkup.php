47c3d872ebed835884aa3a60f7f964537479d814
Zanis3 <euvertzionpagad@gmail.com>
2024-06-01 22:16:25 +0800

    remedy

--- template/appointment-templates/checkup.php
+++ template/appointment-templates/checkup.php
@@ -1,18 +1,20 @@
 <?php
+    require '../config.php';
+    $location = 'dashboard';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style_general.css">
-    <link rel="stylesheet" href="../css/style_appointment.css">
-    <link rel="shortcut icon" href="../img/gen/web-logo.png" type="image/png">
+    <link rel="stylesheet" href="../../css/style_general.css">
+    <link rel="stylesheet" href="../../css/style_appointment.css">
+    <link rel="shortcut icon" href="../../img/gen/web-logo.png" type="image/png">
     <title>Check Up</title>
 </head>
 <body>
-    <?php include_once 'header.php';?>
+    <?php include_once '../header.php';?>
 
-    <?php include_once 'footer.php';?>
+    <?php include_once '../footer.php';?>
 </body>
 </html>
