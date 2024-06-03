cd4911b69d9f4b10e88c504b843c73824ace235d
birdb3446 <birdb3446@gmail.com>
2024-06-03 17:44:25 +0800

    dashboard - renaming

--- dashboard/Dashboard.php
+++ dashboard/Dashboard.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="../css/style_general.css">
     <link rel="stylesheet" href="../css/style_appointment.css">
-    <link rel="stylesheet" href="dash-css/css.css">
+    <link rel="stylesheet" href="../css/dash_style.css">
     <link rel="shortcut icon" href="../img/gen/web-logo.png" type="image/png">
 </head>
 <body>
